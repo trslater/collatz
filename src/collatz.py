401fefee0f9bd75b928553d265477eee61b817e9
trslater <hello@trslater.ca>
2024-03-04 21:41:34 -0800

    Implenent Collatz tree builder

--- src/collatz.py
+++ src/collatz.py
@@ -1,6 +1,21 @@
 from typing import Iterator
 
 
+def build_tree(m: int) -> dict[int, int]:
+    T = {}
+
+    for u in range(m, 0, -1):
+        if u in T:
+            continue
+
+        for v in collatz_iter(u):
+            T[u] = v
+
+            u = v
+
+    return T
+
+
 def collatz_iter(n: int) -> Iterator[int]:
     while True:
         if n == 1:
--- tests/test_collatz.py
+++ tests/test_collatz.py
@@ -1,4 +1,32 @@
-from collatz import collatz_iter
+from collatz import build_tree, collatz_iter
+
+
+def test_build_tree():
+    m = 10
+    expected = {2: 1,
+                3: 10,
+                4: 2,
+                5: 16,
+                6: 3,
+                7: 22,
+                8: 4,
+                9: 28,
+                10: 5,
+                11: 34,
+                13: 40,
+                14: 7,
+                16: 8,
+                17: 52,
+                20: 10,
+                22: 11,
+                26: 13,
+                28: 14,
+                34: 17,
+                40: 20,
+                52: 26}
+    actual = build_tree(m)
+
+    assert(expected == actual)
 
 
 def test_collatz_iter():
