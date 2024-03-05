e2c1d99ec03ceeb14db2f4a7619acae0791b5f97
trslater <hello@trslater.ca>
2024-03-04 21:40:47 -0800

    Improve Collatz iter test

--- tests/test_collatz.py
+++ tests/test_collatz.py
@@ -2,7 +2,9 @@ from collatz import collatz_iter
 
 
 def test_collatz_iter():
-    m = 9
-    expected = [28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
-    
-    assert(all(a == b for a, b in zip(expected, collatz_iter(m))))
+    n = 9
+    expected = [28, 14, 7, 22, 11, 34, 17, 52,
+                26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+    actual = list(collatz_iter(n))
+
+    assert (expected == actual)
